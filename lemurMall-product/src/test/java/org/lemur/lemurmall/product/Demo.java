1a7cbdaf5d557ab4d1a223f5607c6adf04c97f0c
Lemur <lemur@qq.com>
2023-03-12 00:32:50 +0800

    删除敏感信息

--- lemurMall-product/src/test/java/org/lemur/lemurmall/product/Demo.java
+++ lemurMall-product/src/test/java/org/lemur/lemurmall/product/Demo.java
@@ -7,7 +7,6 @@ import com.aliyun.oss.OSSException;
 import com.aliyun.oss.model.PutObjectRequest;
 import com.aliyun.oss.model.PutObjectResult;
 
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -18,8 +17,8 @@ public class Demo {
         // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
         String endpoint = "oss-cn-nanjing.aliyuncs.com";
         // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = "LTAI5t8sMsXHS58winLPxhWm";
-        String accessKeySecret = "9odQTaQbHIs7hOrfp2tEPh7vfdMcY6";
+        String accessKeyId = "";
+        String accessKeySecret = "";
         // 填写Bucket名称，例如examplebucket。
         String bucketName = "lemur-mall";
         // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
